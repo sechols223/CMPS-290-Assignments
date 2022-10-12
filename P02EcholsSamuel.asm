d09010b55907b3477f9a0d60fea0b5309304e9cb
Samuel Echols <samuel.echols@selu.edu>
2022-10-12 18:32:59 -0500

    finish implementing the size loop

--- P02EcholsSamuel.asm
+++ P02EcholsSamuel.asm
@@ -1,15 +1,14 @@
 .data
-	array: .space 20
+	array: .word 0:80 #creates an array of size "80" (it's really 80 / 4) and sets the default value of each element to 0
 	newline: .asciiz "\n"
 	begin: .asciiz "Enter a value between 0 and 20: "
-	continue: .asciiz "Enter the next value: "
+	continue: .asciiz "Enter the desired value: "
 	search: .asciiz "Enter number to search for: "
 	found: .asciiz "The array does contain that number!"
 	notFound: .asciiz "That number does not exist in the array"
-	
 .text
 	main:
-		#Prints a prompt
+		#Prints the beginning  prompt
 		li $v0, 4
 		la $a0, begin
 		syscall		
@@ -17,57 +16,7 @@
 		#User Input
 		li $v0, 5
 		syscall
-		add $s0,$0, $v0
-		
-		li $v0, 4
-		la $a0, continue
-		syscall
-		
-		#User Input
-		li $v0, 5
-		syscall
-		add $s1, $0, $v0
-		
-		li $v0, 4
-		la $a0, continue
-		syscall
-		
-		#User Input
-		li $v0, 5
-		syscall
-		add $s2, $0, $v0
-		
-		li $v0, 4
-		la $a0, continue
-		syscall
-		
-		#User Input
-		li $v0, 5
-		syscall
-		add $s3, $0, $v0
-		
-		li $v0, 4
-		la $a0, continue
-		syscall
-		
-		#User Input
-		li $v0, 5
-		syscall
-		add $s4, $0, $v0
-		
-		sw $s0, array($t0)
-			addi $t0, $t0, 4
-		sw $s1, array($t0)
-			addi $t0, $t0, 4
-		sw $s2, array($t0)
-			addi $t0, $t0, 4
-		sw $s3, array($t0)
-			addi $t0, $t0, 4
-		sw $s4, array($t0)
-			addi $t0, $t0, 4
-		
-		#Resets index to 0
-		addi $t0, $0, 0
+		add $s0, $0, $v0
 		
 		li $v0, 4
 		la $a0, search
@@ -77,9 +26,32 @@
 		li $v0, 5
 		syscall
 		add $s5, $0, $v0
+		
+		
+		
+		
+	whileFirst: 
+		beq $t7, $s0, promptWhile
+		
+		lw $t6, array($t0)
+		addi $t7, $t7, 4
+		
+		li $v0, 4
+		la $a0, continue
+		syscall
+		
+		li $v0, 5
+		syscall
+		add $s1, $zero, $v0
+		
+		sw $s1, array($t0)
+			addi $t0, $t0, 4
+		
+		
+		j whileFirst
 	
-	while:
-		beq $t0, 20, exit
+	promptWhile:
+		beq $t0, 80, exit
 		
 		lw $t6, array($t0)
 		
@@ -87,7 +59,7 @@
 		
 		beq $s5, $t6 equal 
 		 
-		j while
+		j promptWhile
 		
 	exit:
 		li $v0, 4
